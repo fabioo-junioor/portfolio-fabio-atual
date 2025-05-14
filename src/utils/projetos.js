5b302b276a12c38bf742d2c8d2bc116a7fae07ab
Fábio Júnior <fj_011@hotmail.com>
2025-05-14 00:59:55 -0300

    feat: added new project 'gta 6 conutdown' and description project 'trackEase'

--- src/utils/projetos.js
+++ src/utils/projetos.js
@@ -5,7 +5,6 @@ const listProjetos = [
         descricao: 'Sem descrição!',
         techs: ['Wordpress', 'Elementor', 'Woocommerce', 'CSS3', 'Figma'],
         tipo: 'Ecommerce',
-        //imagem: require('../assets/projetos/alquimiaskateshop.png'),
         link: 'https://alquimiaskateshop.com.br/',
         desenvolvimento: false
     },
@@ -15,7 +14,6 @@ const listProjetos = [
         descricao: 'Sem descrição!',
         techs: ['Wordpress', 'Elementor', 'CSS3', 'Figma'],
         tipo: 'Site Institucional',
-        //imagem: require('../assets/projetos/ecologicajr.png'),
         link: 'https://www.ecologicajr.com.br/',
         desenvolvimento: false
     },
@@ -25,7 +23,6 @@ const listProjetos = [
         descricao: 'Sem descrição!',
         techs: ['Wordpress', 'Elementor', 'CSS3', 'Figma'],
         tipo: 'Site Institucional',
-        //imagem: require('../assets/projetos/f5junior.png'),
         link: 'https://f5jr.com/',
         desenvolvimento: false
     },
@@ -35,7 +32,6 @@ const listProjetos = [
         descricao: 'Sem descrição!',
         techs: ['Wordpress', 'Elementor', 'CSS3', 'Figma'],
         tipo: 'Site Institucional',
-        //imagem: require('../assets/projetos/fejers.png'),
         link: 'https://fejers.org.br/',
         desenvolvimento: false
     },
@@ -45,7 +41,6 @@ const listProjetos = [
         descricao: 'Sem descrição!',
         techs: ['Wordpress', 'Elementor', 'CSS3', 'Figma'],
         tipo: 'Site Institucional',
-        //imagem: require('../assets/projetos/nepelin.png'),
         link: 'https://nepelin.com/',
         desenvolvimento: false
     },
@@ -55,7 +50,6 @@ const listProjetos = [
         descricao: 'Sem descrição!',
         techs: ['Wordpress', 'Elementor', 'CSS3', 'Figma'],
         tipo: 'Site Institucional',
-        //imagem: require('../assets/projetos/nucleo-impulso-site.png'),
         link: 'https://nucleoimpulso.com.br/',
         desenvolvimento: false
     },
@@ -65,7 +59,6 @@ const listProjetos = [
         descricao: 'Sem descrição!',
         techs: ['Wordpress', 'Elementor', 'JavaScript', 'CSS3'],
         tipo: 'Site Institucional',
-        //imagem: require('../assets/projetos/patrimonio-cultural-1.png'),
         link: 'https://patrimoniosdonafrancisca.site/',
         desenvolvimento: false
     },
@@ -75,7 +68,6 @@ const listProjetos = [
         descricao: 'Este é um projeto que desenvolvi chamado Ha Assist, é um software desktop que foi idealizada para uma disciplina da faculdade chamada Projeto de Software 2 - UFSM. Todo projeto foi desenvolvido de forma responsiva para melhor adaptação em diversos dispositivos. A ideia principal desse software é facilitar o cadastro de paciêntes que sofrem de hipertensão arterial por parte das infermeiras nos postos de saúde.',
         techs: ['HTML5', 'CSS3', 'JavaScript', 'Bootstrap', 'MySql', 'Electron', 'Figma'],
         tipo: 'Software Desktop',
-        //imagem: require('../assets/projetos/ha-assist.png'),
         link: 'https://github.com/fabioo-junioor/HA--ASSIST',
         repositorio: 'https://github.com/fabioo-junioor/HA--ASSIST',
         desenvolvimento: false
@@ -86,7 +78,6 @@ const listProjetos = [
         descricao: 'Este é um projeto que desenvolvi para servir como portfólio pessoal, se trata de uma aplicação web com um layout intuitivo e atraente que foi desenvolvido de forma responsiva para melhor adaptação em diversos dispositivos. O desenvolvimento do mesmo teve o ituito de praticar funcionalidade e habilidades frontend.',
         techs: ['HTML5', 'CSS3', 'JavaScript', 'Vue.Js', 'Vuetify', 'SASS', 'Animate.css', 'Netlify'],
         tipo: 'Site Pessoal',
-        //imagem: require('../assets/projetos/portfolio2020.png'),
         link: 'https://fabiojuniordev2020.netlify.app/',
         repositorio: 'https://github.com/fabioo-junioor/portfolio-fabio-2020',
         desenvolvimento: false
@@ -97,7 +88,6 @@ const listProjetos = [
         descricao: 'Este é um projeto que desenvolvi para servir como portfólio pessoal, se trata de uma aplicação web com um layout intuitivo e atraente que foi desenvolvido de forma responsiva para melhor adaptação em diversos dispositivos. O desenvolvimento do mesmo teve o ituito de praticar funcionalidade e habilidades frontend.',
         techs: ['HTML5', 'CSS3', 'JavaScript', 'Vue.Js', 'Vuetify', 'SASS', 'Animate.css', 'Netlify'],
         tipo: 'Site Pessoal',
-        //imagem: require('../assets/projetos/portfolio2022.png'),
         link: 'https://fabiojuniordev2022.netlify.app/',
         repositorio: 'https://github.com/fabioo-junioor/portfolio-fabio-2022',
         desenvolvimento: false
@@ -108,7 +98,6 @@ const listProjetos = [
         descricao: 'Este é um projeto que desenvolvi chamado Electoral Partials, é um aplicativo mobile que foi idealizada para uma disciplina da faculdade chamada Projeto e Gerência de Banco de Dados - UFSM. Todo projeto foi desenvolvido de forma responsiva para melhor adaptação em diversos dispositivos. A ideia principal desse aplicativo é proporcinar ao usuário um meio mais eficiênte de divulgação das parciais eleitorais, sem a necessidade de esperar a urna chegar ao TE e os dados seram lançados no site. O usuário que tiver acesso ao boletim de urna podera imediatamente cadastrar os dados e divulga-los.',
         techs: ['Kotlin', 'Java', 'PHP', 'MySql'],
         tipo: 'Aplicativo Mobile',
-        //imagem: require('../assets/projetos/electoral-partials.png'),
         link: 'https://github.com/fabioo-junioor/project-app-electoral-partials',
         repositorio: 'https://github.com/fabioo-junioor/project-app-electoral-partials',
         desenvolvimento: false
@@ -119,7 +108,6 @@ const listProjetos = [
         descricao: 'Este é um projeto que desenvolvi chamado Memórias do passado, é um jogo web que foi idealizada para uma disciplina da faculdade chamada Desenvolvimento de Software Educacional - UFSM. Todo projeto foi desenvolvido de forma responsiva para melhor adaptação em diversos dispositivos. A ideia principal desse jogo é proporcionar ao usuário estudar datas importantes através de um jogo da memória. Toda vez que a partida inicia/reinicia as cartas são sorteadas de forma randômica.',
         techs: ['HTML5', 'CSS3', 'JavaScript', 'Bootstrap',  'Figma', 'Netlify'],
         tipo: 'Jogo da Memória Web',
-        //imagem: require('../assets/projetos/memorias-do-passado.png'),
         link: 'https://memoriasdopassado.netlify.app/',
         repositorio: 'https://github.com/fabioo-junioor/memorias-do-passado',
         desenvolvimento: false
@@ -130,7 +118,6 @@ const listProjetos = [
         descricao: 'Este é um projeto que desenvolvi para servir como portfólio pessoal, se trata de uma aplicação web com um layout intuitivo e atraente que foi desenvolvido de forma responsiva para melhor adaptação em diversos dispositivos. O desenvolvimento do mesmo teve o ituito de praticar funcionalidade e habilidades frontend.',
         techs: ['HTML5', 'CSS3', 'JavaScript', 'Vue.Js', 'BootstrapVue', 'PrimeVue', 'SASS', 'Netlify'],
         tipo: 'Site Pessoal',
-        //imagem: require('../assets/projetos/portfolio2024.png'),
         link: 'https://fabiojuniordev.netlify.app/',
         repositorio: 'https://github.com/fabioo-junioor/portfolio-fabio-atual',
         desenvolvimento: false
@@ -141,7 +128,6 @@ const listProjetos = [
         descricao: 'Este projeto tem como objetivo ajudar na colaboração entre os estudantes da UFSM, através da criação e divulgação de unidades de estudo. Cada unidade de estudo pode compreender diferentes formas de atividades como: cursos, aulas, oficinas, etc, pois os mesmos estarão centralizados em um mesmo lugar. A plataforma está baseada em um sistema de troca que não envolve dinheiro real e sim moedas virtuais. A mesma também dispõe de alguns elementos de gamificação como: moedas virtuais, pontos e ranking, que têm como intuito proporcionar uma maior motivação aos usuários. O usuário que quiser compartilhar algum conhecimento específico, poderá cadastrar sua “unidade” na plataforma para que outros possam adquiri-la, o usuário definirá valor, duração descrição, título, etc.',
         techs: ['HTML5', 'CSS3', 'JavaScript', 'Vue.Js', 'BootstrapVue', 'SASS', 'VueRouter', 'Bcryptjs', 'PHP', 'MySql', 'Figma', 'Netlify'],
         tipo: 'Aplicação Web',
-        //imagem: require('../assets/projetos/uniconecta.png'),
         link: 'https://uniconecta.netlify.app/',
         repositorio: 'https://github.com/fabioo-junioor/uniConecta',
         desenvolvimento: false
@@ -152,7 +138,6 @@ const listProjetos = [
         descricao: 'Este é um projeto que desenvolvi chamado Pokédex, é uma aplicação web que consome dados de uma API e os exibe em um layout intuitivo e atraente. Todo projeto foi desenvolvido de forma responsiva para melhor adaptação em diversos dispositivos. A Pokédex é uma ferramenta para explorar e descobrir informações sobre diferentes espécies de Pokémon. Utilizando a API pública chamada pokéApi, o projeto oferece uma interface amigável para visualizar detalhes sobre os Pokémon, como suas estatísticas, habilidades e tipos.',
         techs: ['HTML5', 'CSS3', 'JavaScript', 'Vue.Js', 'PrimeVue', 'SASS', 'PokeApi', 'Netlify'],
         tipo: 'Aplicação Web',
-        //imagem: require('../assets/projetos/pokedex.png'),
         link: 'https://pokkkedex.netlify.app/',
         repositorio: 'https://github.com/fabioo-junioor/pokedex',
         desenvolvimento: false
@@ -163,7 +148,6 @@ const listProjetos = [
         descricao: 'Este é um projeto que desenvolvi chamado Monty Hall, é um jogo web que foi idealizada para praticar o desenvolvimento frontend. Todo projeto foi desenvolvido de forma responsiva para melhor adaptação em diversos dispositivos. A ideia principal desse jogo é proporcionar ao usuário a definição de quantas portas haverá e posteriormente o mesmo acertar em qual delas á um presente. Toda vez que a partida inicia/reinicia o presente é sorteado de forma randômica entre as portas.',
         techs: ['HTML5', 'CSS3', 'JavaScript', 'Vue.Js', 'Netlify'],
         tipo: 'Aplicação Web',
-        //imagem: require('../assets/projetos/monty-hall.png'),
         link: 'https://montyyhall.netlify.app/',
         repositorio: 'https://github.com/fabioo-junioor/monty-hall',
         desenvolvimento: false
@@ -174,7 +158,6 @@ const listProjetos = [
         descricao: 'Este é um projeto que desenvolvi para praticar funcionalidades javascrip e manipulação do DOM, se trata de uma aplicação web com um layout intuitivo e atraente que foi desenvolvido de forma responsiva para melhor adaptação em diversos dispositivos. O desenvolvimento do mesmo teve o ituito de praticar habilidades frontend.',
         techs: ['HTML5', 'CSS3', 'JavaScript', 'Vue.Js', 'Netlify'],
         tipo: 'Aplicação Web',
-        //imagem: require('../assets/projetos/calculadora.png'),
         link: 'https://ccalculadora.netlify.app/',
         repositorio: 'https://github.com/fabioo-junioor/calculadora',
         desenvolvimento: false
@@ -185,7 +168,6 @@ const listProjetos = [
         descricao: 'Este é um projeto que desenvolvi chamado Game Rush, é um jogo web que foi idealizada para uma disciplina da faculdade chamada Paradigmas de Programação - UFSM. A ideia principal desse jogo é proporcionar ao usuário diversão enquanto o mesmo conduz o estudante na busca por conhecimento conletando livros ao longo do percurso. O estudante deverá coletar um numero determinado de livros até o final do percurso.',
         techs: ['Java', 'Libgdx', 'Itch.io'],
         tipo: 'Jogo Web',
-        //imagem: require('../assets/projetos/game-hush.png'),
         link: 'https://julianopasa.itch.io/procrastinator-rush',
         repositorio: 'https://github.com/fabioo-junioor/game-rush',
         desenvolvimento: false
@@ -196,7 +178,6 @@ const listProjetos = [
         descricao: 'Este é um projeto que desenvolvi chamado Mesa Online, é uma aplicação web que consome dados de uma API desenvolvida em PHP e os exibe em um layout intuitivo e atraente criado em VueJs. Todo projeto foi desenvolvido de forma responsiva para melhor adaptação em diversos dispositivos. A Mesa Online é uma ferramenta que foi desenvolvida para proporcionar aos usuários reservarem lugares em estabelecimento comercias como: restaurantes, pubs, bares, etc. Os estabelecimentos podem efetuar seu cadastro e disponibilizar informções pertinentes para que usuário possam reservar um os mais lugares com rapidez sem a necessidade de esperar o retorno de um funcionário.',
         techs: ['HTML5', 'CSS3', 'JavaScript', 'Vue.Js', 'SASS', 'Quasar', 'VueRouter', 'Vuex', 'Chart.Js', 'Node.Js', 'Express.Js', 'Jwt', 'MySql', 'Netlify'],
         tipo: 'Aplicação Web',
-        //imagem: require('../assets/projetos/mesa-online.png'),
         link: 'https://mesaonline.netlify.app/',
         repositorio: 'https://github.com/fabioo-junioor/mesa-online',
         desenvolvimento: true
@@ -206,8 +187,7 @@ const listProjetos = [
         titulo: 'Facemash',
         descricao: 'Este é um projeto que desenvolvi chamado Facemash, é uma aplicação web que consome dados de uma API e os exibe em um layout intuitivo e atraente. Todo projeto foi desenvolvido de forma responsiva para melhor adaptação em diversos dispositivos. O Facemash é uma ferramenta para o usuário votar no seu personagem favorito da saga Harry Potter. Utilizando a API pública chamada potterDB, o projeto oferece uma interface amigável para visualizar detalhes sobre os personagens, com seu nome e espécie. Cada voto é computado e salvo em um banco de dados no firebase.',
         techs: ['HTML5', 'CSS3', 'JavaScript', 'Vue.Js', 'SASS', 'Firebase', 'Netlify'],
-        tipo: 'Aplicação web',
-        //imagem: require('../assets/projetos/facemash.png'),
+        tipo: 'Aplicação Web',
         link: 'https://ffacemash.netlify.app/',
         repositorio: 'https://github.com/fabioo-junioor/facemash',
         desenvolvimento: true
@@ -217,8 +197,7 @@ const listProjetos = [
         titulo: 'Deep Space Store',
         descricao: 'Este é um projeto que desenvolvi chamado Deep Space Store, é uma aplicação web que tem por objetivo proporcionar ao usuário um fluxo de checkout de um produto aleatório. Todo projeto foi desenvolvido de forma responsiva para melhor adaptação em diversos dispositivos. O Deep Space Store é uma ferramenta para o usuário selecionar um produto e posteriormente preencher seu dados de compra em 3 formulários. O projeto oferece uma interface amigável para visualizar detalhes sobre a compra, com seu nome, valor e descrição. Todos os formulários passam por uma validação em seus dados. As requisições são mockadas com a utilização do msw.',
         techs: ['HTML5', 'CSS3', 'JavaScript', 'Vue.Js', 'SASS', 'Vuetify', 'VueRouter', 'Vuex', 'Msw', 'Webdriver', 'Cypress', 'Netlify'],
-        tipo: 'Aplicação web',
-        //imagem: require('../assets/projetos/deep-space-store.png'),
+        tipo: 'Aplicação Web',
         link: 'https://deep-space-store.netlify.app/',
         repositorio: 'https://github.com/fabioo-junioor/deep-space-store',
         desenvolvimento: false
@@ -228,8 +207,7 @@ const listProjetos = [
         titulo: 'Hora do Corte',
         descricao: 'Este é um projeto que desenvolvi chamado Hora do Corte, é uma aplicação web que consome dados de uma API desenvolvida em Node.js e os exibe em um layout intuitivo e atraente criado em Vue.js. Todo projeto foi desenvolvido de forma responsiva para melhor adaptação em diversos dispositivos. A Hora do Corte é uma ferramenta que foi desenvolvida para proporcionar aos usuários reservarem horários em estabelecimento comercias como: salões de beleza e barbearias. Os estabelecimentos podem efetuar seu cadastro e disponibilizar informções pertinentes para que usuário possam reservar um horário. O usuário pode definir o "funcionário, serviço(os) e horário". ',
         techs: ['HTML5', 'CSS3', 'JavaScript', 'Vue.Js', 'SASS', 'Quasar', 'VueRouter', 'Vuex', 'Chart.Js', 'Node.Js', 'Express.Js', 'Jwt', 'MySql', 'Netlify', 'Render'],
-        tipo: 'Aplicação web',
-        //imagem: require(''),
+        tipo: 'Aplicação Web',
         link: 'https://horadocorte.netlify.app/',
         repositorio: 'https://github.com/fabioo-junioor/hora-do-corte-frontend',
         desenvolvimento: false
@@ -237,13 +215,22 @@ const listProjetos = [
     {
         id: 23,
         titulo: 'TrackEase',
-        descricao: 'Sem descrição!',
+        descricao: 'Este é um projeto que desenvolvi chamado TrackEase, é uma aplicação web que consome dados de uma API desenvolvida em Node.js e os exibe em um layout intuitivo e atraente criado em Vue.js. Todo projeto foi desenvolvido de forma responsiva para melhor adaptação em diversos dispositivos. O TrackEase é uma aplicação/framework que foi desenvolvida para proporcionar ao usuário/desenvolvendor monitorar as métricas de usabilidade de seus projetos. Essas métricas são capturadas através de eventos gerados pelo usuário, com a interação do mesmo com a interfaces web.',
         techs: ['HTML5', 'CSS3', 'JavaScript', 'Vue.Js', 'SASS', 'Quasar', 'VueRouter', 'Vuex', 'Chart.Js', 'Node.Js', 'Express.Js', 'Jwt', 'MySql', 'Netlify', 'Render', 'Aiven'],
-        tipo: 'Aplicação web/Framework',
-        //imagem: require(''),
+        tipo: 'Aplicação Web/Framework',
         link: 'https://track-ease.netlify.app/',
-        repositorio: '',
+        repositorio: 'https://github.com/fabioo-junioor/track-ease-frontend',
         desenvolvimento: true
+    },
+    {
+        id: 24,
+        titulo: 'Contagem Regressiva GTA 6',
+        descricao: 'Este é um projeto de página promocional estática desenvolvida para destacar o lançamento do aguardado jogo GTA 6. Criado com foco em uma apresentação visual atrativa e responsiva, o site traz informação do tempo restante para o lançamento do jogo, servindo como uma central de divulgação para fãs e curiosos.',
+        techs: ['HTML5', 'CSS3', 'JavaScript', 'Netlify'],
+        tipo: 'Página Web',
+        link: 'https://lancamentogta6.netlify.app/',
+        repositorio: 'https://github.com/fabioo-junioor/gta6-countdown',
+        desenvolvimento: false
     }
 ];
 
